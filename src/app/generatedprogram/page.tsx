4d069acb01e1b8b702e5a19352ab7da1a0391d75
PasanSasmika <pasansasmika198@gmail.com>
2025-06-09 08:46:13 +0530

    Project Setup

---
+++ src/app/generatedprogram/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function GeneratedProgram() {
+  return (
+    <div>GeneratedProgram</div>
+  )
+}
+
+export default GeneratedProgram
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-function Home() {
-  
+function Profile() {
   return (
     <div>page</div>
   )
 }
 
-export default Home
+export default Profile
